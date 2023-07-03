fce2ce48af13537656ba09a6e531a9201c460e9f
agimenez91 <andrea.gimenez91@gmail.com>
2023-07-03 13:04:43 +0200

    Routes configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import './globalStyles/GlobalStyle.scss'
+import Home from './views/Home/Home'
+import PokemonView from './views/PokemonView/PokemonView'
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:pokemonID" element={<PokemonView />} />
+      </Routes>
     </>
   )
 }
