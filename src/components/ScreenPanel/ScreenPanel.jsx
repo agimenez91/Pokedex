fa115637fa899bf26108cc88d8e6b98f7a1b8121
agimenez91 <andrea.gimenez91@gmail.com>
2023-07-04 12:29:53 +0200

    pokemon views added and styled

--- src/components/ScreenPanel/ScreenPanel.jsx
+++ src/components/ScreenPanel/ScreenPanel.jsx
@@ -1,10 +1,12 @@
+import PokemonCard from '../PokemonCard/PokemonCard';
+import PokemonView from '../PokemonView/PokemonView';
 import './ScreenPanel.scss';
 
 export default function ScreenPanel (props) {
     return (
         <>
             <div className="screenpannel">
-                <h1>ScreenPannel</h1>
+                <PokemonView/>
             </div>
         </>
     )
